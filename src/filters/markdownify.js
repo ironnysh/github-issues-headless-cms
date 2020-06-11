3b0277b68ff655fc18a7b4fd3877f291351aefd1
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2020-06-11 22:28:15 +0100

    changing markdown-it settings to close img tags etc for rss validation

--- src/filters/markdownify.js
+++ src/filters/markdownify.js
@@ -2,6 +2,7 @@ const md = require("markdown-it")({
   html: false,
   breaks: true,
   linkify: true,
+  xhtmlOut: true,
 });
 module.exports = function markdownify(markdownString) {
   return md.render(markdownString);
