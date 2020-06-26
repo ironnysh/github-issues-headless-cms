df913c5db92b7b411e5b1e572187870b8f61c597
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2020-06-26 22:47:42 +0100

    neaten up date filter

--- src/filters/date-filter.js
+++ src/filters/date-filter.js
@@ -1,8 +1,6 @@
 const { DateTime } = require("luxon");
 const siteSettings = require("../_data/site.js");
-const locale = siteSettings.locale.locale;
-// const locale = "UTC";
-const timeZone = siteSettings.locale.timeZone;
+const { locale, timeZone } = siteSettings.locale;
 
 const appendDateSuffix = (n) => {
   const s = ["th", "st", "nd", "rd"],
@@ -10,14 +8,6 @@ const appendDateSuffix = (n) => {
   return n + (s[(v - 20) % 10] || s[v] || s[0]);
 };
 
-// module.exports = (value) => {
-//   const dateObject = DateTime.fromISO(value);
-
-//   const formattedDate = appendDateSuffix(dateObject.toFormat("d"));
-//   const formattedMonthYear = dateObject.toFormat("MMMM yyyy");
-//   const formattedTime = dateObject.toFormat("T");
-//   return `${formattedDate} ${formattedMonthYear} at ${formattedTime}`;
-// };
 module.exports = (value) => {
   const dateObject = DateTime.fromISO(value, {
     locale: locale,
