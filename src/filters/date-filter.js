1e0eb2b601fb51ebc5dcb7bc74866cebce9615ec
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2020-06-26 22:31:13 +0100

    testing new date filter with luxon and no locale set

--- src/filters/date-filter.js
+++ src/filters/date-filter.js
@@ -1,38 +1,30 @@
-const appendSuffix = (n) => {
-  var s = ["th", "st", "nd", "rd"],
+const { DateTime } = require("luxon");
+const siteSettings = require("../_data/site.js");
+// const locale = siteSettings.locale.locale;
+// const locale = "UTC";
+const timeZone = siteSettings.locale.timeZone;
+
+const appendDateSuffix = (n) => {
+  const s = ["th", "st", "nd", "rd"],
     v = n % 100;
   return n + (s[(v - 20) % 10] || s[v] || s[0]);
 };
 
-const addZero = (number) => {
-  if (number < 10) {
-    number = "0" + number;
-  }
-  return number;
+module.exports = (value) => {
+  const dateObject = DateTime.fromISO(value);
+
+  const formattedDate = appendDateSuffix(dateObject.toFormat("d"));
+  const formattedMonthYear = dateObject.toFormat("MMMM yyyy");
+  const formattedTime = dateObject.toFormat("T");
+  return `${formattedDate} ${formattedMonthYear} at ${formattedTime}`;
 };
+// module.exports = (value) => {
+//   const dateObject = DateTime.fromISO(value);
 
-module.exports = function dateFilter(value) {
-  const dateObject = new Date(value);
-
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  const dayWithSuffix = appendSuffix(dateObject.getDate());
-
-  return `${dayWithSuffix} ${
-    months[dateObject.getMonth()]
-  } ${dateObject.getFullYear()} at ${addZero(dateObject.getHours())}:${addZero(
-    dateObject.getMinutes()
-  )}`;
-};
+//   const formattedDate = appendDateSuffix(
+//     dateObject.setLocale(locale).toFormat("d")
+//   );
+//   const formattedMonthYear = dateObject.setLocale(locale).toFormat("MMMM yyyy");
+//   const formattedTime = dateObject.setLocale(locale).toFormat("T");
+//   return `${formattedDate} ${formattedMonthYear} at ${formattedTime}`;
+// };
