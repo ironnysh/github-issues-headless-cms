40092979c5a9375f54e4dfdd535b9cc40e1e177a
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2020-06-28 22:26:58 +0100

    add favicon, break out colours as data file

--- src/transforms/markdownImagesToCloudinary.js
+++ src/transforms/markdownImagesToCloudinary.js
@@ -1,9 +1,16 @@
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
-const Cloudinary_Cloud_Name = require("../_data/site.js")
-  .Cloudinary_Cloud_Name;
+// const probeImage = require("probe-image-size");
+const Cloudinary_Cloud_Name = require("../_data/site.js").Cloudinary_Cloud_Name;
 
-module.exports = function (value, outputPath) {
+module.exports = async function (value, outputPath) {
+  // async function getImageDimensions(imageURL) {
+  //   const response = probeImage(imageURL).then(function (response) {
+  //     return response;
+  //   });
+  //   return response;
+  // }
+
   if (outputPath.endsWith(".html")) {
     const DOM = new JSDOM(value, {
       resources: "usable",
@@ -19,6 +26,21 @@ module.exports = function (value, outputPath) {
         const originalSource = image.getAttribute("src");
 
         const newSource = `https://res.cloudinary.com/${Cloudinary_Cloud_Name}/image/fetch/f_auto/${originalSource}`;
+
+        // const imageDimensions = (async () => {
+        //   const { width, height } = await getImageDimensions(originalSource);
+
+        //   return [width, height];
+        // })();
+
+        // const data = async (originalSource) => {
+        //   const imageDimensions = await getImageDimensions(originalSource);
+        //   return imageDimensions;
+        // };
+        // console.log(data);
+
+        // image.setAttribute("width", width);
+        // image.setAttribute("height", height);
 
         image.setAttribute("src", newSource);
       });
