028221772e05526e4e96a365155f5ae45719b199
ironnysh <63248335+ironnysh@users.noreply.github.com>
2020-07-19 19:28:47 +0200

    remove stuff in date filter, dependencies package

--- .github/ISSUE_TEMPLATE/Fm-post.md
+++
@@ -1,8 +0,0 @@
----
-name: Front matter post
-about: A post for immediate publication with front matter
-title: ''
-labels: ''
-translator: ''
-publisher: ''
----
--- src/filters/date-filter.js
+++ src/filters/date-filter.js
@@ -2,19 +2,11 @@ const { DateTime } = require("luxon");
 const siteSettings = require("../_data/siteSettings.js");
 const timeZone = siteSettings.timeZone || "Europe/London";
 
-const appendDateSuffix = (n) => {
-  const s = ["th", "st", "nd", "rd"],
-    v = n % 100;
-  return n + (s[(v - 20) % 10] || s[v] || s[0]);
-};
-
 module.exports = (value) => {
   const dateObject = DateTime.fromISO(value, {
     zone: timeZone,
   });
 
-  const formattedDate = appendDateSuffix(dateObject.toFormat("d"));
-  const formattedMonthYear = dateObject.toFormat("MMMM yyyy");
-  const formattedTime = dateObject.toFormat("T");
-  return `${formattedDate} ${formattedMonthYear} at ${formattedTime}`;
+  const formattedDate = dateObject.toFormat("MMMM d, yyyy");
+  return `${formattedDate}`;
 };
